72dafab7a5527cdeeabf7433e3395a7ad4d967aa
ashish-pro <ashishbit017@outlook.com>
2024-02-05 00:06:01 +0530

    dynamic sliders using strapi backend

--- App/Components/Home/Slider.js
+++ App/Components/Home/Slider.js
@@ -1,19 +1,32 @@
 import { View, Text, FlatList, Image, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GlobalAPI from '../../Services/GlobalAPI';
 
 const Slider = () => {
-    const sliderList = [
-        {
-            id: 1,
-            name: 'Slider 1',
-            imageUrl: 'https://img.freepik.com/free-photo/medium-shot-scientists-posing-together_23-2148969982.jpg?size=626&ext=jpg&ga=GA1.1.282912230.1706962609&semt=sph'
-        },
-        {
-            id: 2,
-            name: 'Slider 2',
-            imageUrl: 'https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?size=626&ext=jpg&ga=GA1.1.282912230.1706962609&semt=sph'
-        }      
-    ]
+  const [sliderList,setSliderList] = useState();
+    // const sliderList = [
+    //     {
+    //         id: 1,
+    //         name: 'Slider 1',
+    //         imageUrl: 'https://img.freepik.com/free-photo/medium-shot-scientists-posing-together_23-2148969982.jpg?size=626&ext=jpg&ga=GA1.1.282912230.1706962609&semt=sph'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Slider 2',
+    //         imageUrl: 'https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?size=626&ext=jpg&ga=GA1.1.282912230.1706962609&semt=sph'
+    //     }      
+    // ]
+    useEffect(()=>{
+      getSlider();
+    },[]);
+
+    const getSlider = ()=>{
+      GlobalAPI.getSlider().then(res =>{
+        console.log(res.data.data);
+        setSliderList(res.data.data);
+      })
+    }
+
   return (
     <View style={{marginTop:10}}>
       <FlatList 
@@ -21,9 +34,9 @@ const Slider = () => {
         horizontal={true}
         showsHorizontalScrollIndicator={false}
         renderItem={({item})=> (
-            <Image source={{uri:item.imageUrl}}
+            <Image source={{uri:item.attributes.Image.data.attributes.url}}
             style={{width:Dimensions.get('screen').width*0.9,
-        height:220, borderRadius:10, margin:2}} />
+        height:190, borderRadius:10, margin:2}} />
         )}
       />
     </View>
---
+++ App/Services/GlobalAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "http://192.168.1.20:1337/api"
+
+const API_KEY = "12b2cb7f7008443d2d60de1f8468f2c056a0c9e554adaf861f7bf9fc0f81d61e894bc6a405f0fe17739836c628baea95954261acb10836e9ec386df9ad1e61ea267006b4bc63f7953198f8ac01dcdaab7df99d581eba0423d3b7aa6f7e47a449cf7a3957f44f38aad9369592e6f84973104e275e7762c5b2e25185315c23ec86"
+
+const AxiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Authorization': "Bearer" + API_KEY
+    }
+});
+
+const getSlider = () => AxiosInstance.get("/sliders?populate=*");
+
+export default {
+    getSlider
+}
